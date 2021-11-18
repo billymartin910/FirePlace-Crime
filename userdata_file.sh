6e35dbbfcd3425bb7bb338f77b94eb151501928a
BillyMartin <BillyMartin910@gmail.com>
2021-11-18 21:25:22 +0000

    userdata typo

--- userdata_file.sh
+++ userdata_file.sh
@@ -7,9 +7,14 @@ sudo yum install nginx -y
 sudo yum install mariadb-server mariadb -y
 sudo yum install wget -y 
 sudo yum install unzip -y 
+sudo wget https://wordpress.org/latest.zip
+sudo unzip latest.zip
 sudo mv wordpress/* /var/www/html/
-sudo wget http://rpms.remirepo.net/enterprise/remi-release-7.rpm 
+
+#Below are the commands to install php version 7
+sudo yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y
 sudo yum-config-manager --enable remi-php72 -y
+sudo yum-config-manager --enable remi-php72
 sudo yum install php -y
 sudo yum install php php-mysql -y
 sudo setenforce 0
