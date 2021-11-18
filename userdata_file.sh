325f5d7560aabe408847a91fc092a36888d89cf3
BillyMartin <BillyMartin910@gmail.com>
2021-11-18 21:07:40 +0000

    added php v7

--- userdata_file.sh
+++ userdata_file.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 yum install httpd -y 
 yum install telnet -y 
-sudo systemctl start httpd
-sudo systemctl enable httpd
 sudo yum install nginx -y 
 sudo yum install mariadb-server mariadb -y
+sudo yum install wget -y 
+sudo yum install unzip -y 
+sudo wget http://rpms.remirepo.net/enterprise/remi-release-7.rpm 
+sudo yum-config-manager --enable remi-php72 -y
+sudo yum install php -y
+sudo yum install php php-mysql -y
+sudo systemctl start httpd
+sudo systemctl enable httpd
+sudo setenforce 0
 
