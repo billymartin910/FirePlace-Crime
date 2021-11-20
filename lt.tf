9b22913ccb7ebd274399a8c667171d73677732ad
BillyMartin <BillyMartin910@gmail.com>
2021-11-20 19:53:11 +0000

    added az_data.tf

--- lt.tf
+++ lt.tf
@@ -1,5 +1,5 @@
-  resource "aws_launch_template" "fireplace_crime" {
-    name_prefix = "example"
-    image_id = "${data.aws_ami.ubuntu.id}"
-    instance_type = "t2.micro"
-  }
+resource "aws_launch_template" "fireplace_crime" {
+  name_prefix   = "example"
+  image_id      = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+}
--- output.tf
+++ output.tf
@@ -1,4 +1,7 @@
 output "aws_id" {
-  value = "${aws_instance.fireplace_crime.id}"
+  value = aws_instance.fireplace_crime.id
 }
 
+output "available_zone" {
+  value = "${data.aws_availability_zones.available.names[0]}"
+}
